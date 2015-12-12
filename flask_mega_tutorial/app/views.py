3632f0a485ff0526f88ee195b95c95378728dcf4
Lars Hedlund <lkhedlund@gmail.com>
2015-12-11 16:23:06 -0800

    Basic MegaFlask tutorial structure complete

--- flask_mega_tutorial/app/views.py
+++ flask_mega_tutorial/app/views.py
@@ -3,4 +3,7 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    user = {'username': 'Lars'}
+    return render_template('index.html',
+                            title='Home',
+                            user=user)
